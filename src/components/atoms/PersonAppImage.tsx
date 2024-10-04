b651223476b2f759013c4a2ab743f665bf22ca70
Alexis Orellana <1005alexis@gmail.com>
2024-10-04 12:41:08 -0600

    WEB KAN-96 Implement Intro Slider Component

--- src/components/atoms/PersonAppImage.tsx
+++ src/components/atoms/PersonAppImage.tsx
@@ -1,9 +1,16 @@
-import initialScreenImage from './../../assets/Main_screen_img_bg.svg';
+import React from 'react';
+import initialScreenImage from './../../assets/images/intro_sliders/intro_1.png';
 
-export const PersonAppImage = () => {
+type PersonAppImageProps = {
+  imageSrc?: string;
+};
+
+export const PersonAppImage: React.FC<PersonAppImageProps> = ({
+  imageSrc = initialScreenImage,
+}) => {
   return (
     <img
-      src={initialScreenImage}
+      src={imageSrc}
       alt="Initial screen"
       style={{
         borderRadius: '5%',
