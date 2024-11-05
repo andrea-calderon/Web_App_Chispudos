6fd536acab5774e54551804f151e934a3ab8f49e
Alexis Orellana <1005alexis@gmail.com>
2024-11-04 20:35:35 -0700

    KAN-107: fixed merge conflicts

--- src/features/home/components/pages/HomePage.tsx
+++ src/features/home/components/pages/HomePage.tsx
@@ -1,18 +1,105 @@
-// import { useAppSelector } from '../../../../hooks/useAppSelector';
-// import { useGetExampleDataQuery } from '../../../../services/api';
-// import { selectAuth } from '../../../../redux/slices/authSlice';
 import { UserLayout } from '../../../../components/templates/UserLayout';
+import { TextAtom } from '../../../../components/atoms';
+import ServicesCard from '../../../../components/atoms/ServicesCard';
+import { Box, Grid2 } from '@mui/material';
+import Footer from '../../../../components/organisms/Footer';
+import HighlightedCategories from '../organisms/Categories';
 import SearchBar from '../../../../components/organisms/SearchBar';
 
-export const HomePage = () => {
-  // const authState = useAppSelector(selectAuth);
-  // const { data } = useGetExampleDataQuery();
+
+type ServicesSection = {
+  name: string;
+  image: string;
+  pricePerHour: string;
+  rating: number;
+  reviewCount: number;
+  jobsInQueue: number;
+  [key: string]: unknown;
+};
+
+export const HomePage: React.FC = () => {
+
+  const services: ServicesSection[] = [
+    {
+      name: 'José Perez',
+      image: 'src/assets/images/services/Jose_Perez.svg',
+      pricePerHour: 'Q30',
+      rating: 4.5,
+      reviewCount: 4,
+      jobsInQueue: 2,
+    },
+    {
+      name: 'Roberto Castro',
+      image: 'src/assets/images/services/Roberto_Castro.svg',
+      pricePerHour: 'Q18',
+      rating: 4.6,
+      reviewCount: 5,
+      jobsInQueue: 3,
+    },
+    {
+      name: 'Juan García',
+      image: 'src/assets/images/services/Juan_Garcia.svg',
+      pricePerHour: 'Q18',
+      rating: 4.5,
+      reviewCount: 4,
+      jobsInQueue: 3,
+    },
+    {
+      name: 'Carlos Blanco',
+      image: 'src/assets/images/services/Carlos_Blanco.svg',
+      pricePerHour: 'Q18',
+      rating: 4.5,
+      reviewCount: 4,
+      jobsInQueue: 3,
+    },
+    {
+      name: 'Pedro Martínez',
+      image: 'src/assets/images/services/Pedro_Martinez.svg',
+      pricePerHour: 'Q18',
+      rating: 4.5,
+      reviewCount: 4,
+      jobsInQueue: 1,
+    },
+  ];
+
   return (
     <UserLayout>
-      <h1>Home Page</h1>
       <SearchBar />
-      {/* <pre>{JSON.stringify(authState, null, 2)}</pre>
-      <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <TextAtom variant="display" size="medium" fontWeight="bold">
+        Home Page
+      </TextAtom>
+      <HighlightedCategories />
+      <Box sx={{ position: 'relative', padding: 4 }}>
+        <TextAtom
+          variant="title"
+          size="large"
+          fontWeight="bold"
+          marginBottom={2}
+        >
+          Recomendados para ti
+        </TextAtom>
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
+        <Grid2
+          container
+          spacing={2}
+          sx={{
+            overflowX: 'auto',
+            flexWrap: 'nowrap',
+            scrollBehavior: 'smooth',
+            flexGrow: 1,
+            width: '100%',
+          }}
+        >
+          {services.map((service, index) => (
+            <Grid2 key={index}>
+              <ServicesCard {...service} />
+            </Grid2>
+          ))}
+        </Grid2>
+      </Box>
+      <Footer />
     </UserLayout>
   );
 };
