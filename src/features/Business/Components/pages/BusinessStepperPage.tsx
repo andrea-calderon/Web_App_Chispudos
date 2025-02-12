2b64b8f2a69805a3ff8281081b1953d1a56d39b0
Rolando Orellana <orellana.marketing@gmail.com>
2025-02-12 15:30:17 -0600

    KAN-144 code structure for step 5 implemented

--- src/features/Business/Components/pages/BusinessStepperPage.tsx
+++ src/features/Business/Components/pages/BusinessStepperPage.tsx
@@ -1,10 +1,12 @@
 import { UserLayout } from '../../../../components/templates/UserLayout';
 import Step4 from '../Organisms/Step4';
+import Step5 from '../Organisms/Step5';
 
 export const BusinessStepper = () => {
   return (
     <UserLayout>
       <Step4 />
+      <Step5 />
     </UserLayout>
   );
 };
