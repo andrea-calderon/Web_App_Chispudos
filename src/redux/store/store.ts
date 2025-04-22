e7940edb68db87d2f47e3edb9a19a8601e5f57eb
Rolando Orellana <orellana.marketing@gmail.com>
2025-04-22 16:05:53 -0600

    KAN-157 modeSlice added and store updated

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage'; // defaults to localStorage for
 import { combineReducers } from 'redux';
 import authReducer from '../slices/authSlice';
 import serviceStepperReducer from '../slices/serviceStepperSlice';
+import modeReducer from '../slices/modeSlice';
 import { authApi } from '../../services/authApi';
 import { productApi } from '../../services/productApi';
 import { categoryApi } from '../../services/categoryApi';
@@ -15,6 +16,7 @@ import { chatApi } from '../../services/chatApi';
 const rootReducer = combineReducers({
   auth: authReducer,
   serviceStepper: serviceStepperReducer,
+  mode: modeReducer, 
   [authApi.reducerPath]: authApi.reducer,
   [productApi.reducerPath]: productApi.reducer,
   [categoryApi.reducerPath]: categoryApi.reducer,
@@ -28,7 +30,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['auth', 'serviceStepper'], // Only persist the auth reducer
+  whitelist: ['auth', 'serviceStepper', 'mode'], // Only persist the auth reducer
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
